311478ac17e7f266eef141f577a172cd952478a9
Махаличев Никита <54910038+mahalichev@users.noreply.github.com>
2023-02-14 20:11:39 +0300

    Delete scripts directory

--- scripts/APIs/github/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .github import GitHub
-from .right_access_manager import RightAccessManager
-from .comment_manager import CommentManager
-from .readme_manager import ReadmeManager
-from .pull_request_manager import PullRequestManager
