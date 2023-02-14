5b4666c67f28030fe7053ee406715fa13216f4ea
Махаличев Никита <54910038+mahalichev@users.noreply.github.com>
2023-02-14 20:12:17 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,2 +0,0 @@
-docker rm -f pr-automation
-docker run -d --network=host --restart always --name pr-automation -e RUNNER_NAME="pr-automation" -e RUNNER_GROUP="bot" -e ACCESS_TOKEN="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" -e RUNNER_SCOPE="org" -e ORG_NAME="devspring2022-org" -e PR_BOT_CONFIG_PATH="D:\Templates\config" -v /var/run/docker.sock:/var/run/docker.sock myoung34/github-runner:latest
